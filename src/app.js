f71cd04950dc2a6869e571996ca3881a31caec77
karinasoarex <lkarinas.silva@gmail.com>
2021-02-12 16:17:48 -0300

    update backend

---
+++ src/app.js
@@ -0,0 +1,28 @@
+//importa o express
+const express = require("express");
+const { errors } = require("celebrate");
+
+//importa o cors
+const cors = require("cors");
+
+//importa as rotas
+const routes = require("./routes");
+
+require("./database");
+
+//cria a aplicação express
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+//definimos a pasta uploads como pública, servindo arquivos estáticos
+app.use("/uploads", express.static("uploads"));
+
+app.use(routes);
+
+app.use(errors());
+
+
+module.exports = app;
